a76cc1c5e43740f6ff20468aad913b61796e6801
Maxipar03 <maxipar2003@gmail.com>
2022-09-19 03:40:47 -0300

    Agregando link de productos a editar,modificacion vista del index y de validaciones

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,11 +3,11 @@ let { body } = require('express-validator');
 const validation = [ 
 
     body("name").notEmpty().withMessage("Este campo no puede estar vacio").bail()
-    .isLength({min:5, max:10}).withMessage("El nombre debe tener entre 5 y 10 caracteres"),
+    .isLength({min:5, max:60}).withMessage("El nombre debe tener entre 5 y 60 caracteres"),
     body("price").notEmpty().withMessage("Este campo no puede estar vacio").bail()
     .isNumeric().withMessage('El precio debe contener solo números'),
     body("description").notEmpty().withMessage("Este campo no puede estar vacio").bail()
-    .isLength({min:30, max:300}).withMessage("La descripcion debe tener entre 30 y 300 caracteres"),
+    .isLength({min:10, max:300}).withMessage("La descripcion debe tener entre 10 y 300 caracteres"),
     body("image").notEmpty().withMessage("Este campo no puede estar vacio").bail()
     .isURL({protocols: ['http','https','ftp']}).withMessage("Debes ingresar una url valida"),
 ]
